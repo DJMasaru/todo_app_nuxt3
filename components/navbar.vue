0f576bb72e49d3c5b97a074e6f157498b09f373b
DJMasaru <otsusyarepapa@gmail.com>
2023-12-31 08:28:46 +0900

    インデント

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,8 +1,17 @@
 <template>
   <div style="height: 70px; display: flex; align-items: center; justify-content: center">
     <nav>
-      <v-btn href="/" style="width: 100px;margin-left: 5px;margin-right: 5px">Home</v-btn>
-      <v-btn href="/addTodo" style="width: 100px;margin-left: 5px;margin-right: 5px">Add</v-btn>
+      <v-btn
+          href="/"
+          style="width: 100px;margin-left: 5px;margin-right: 5px"
+      >Home
+      </v-btn>
+      <v-btn
+          href="/addTodo"
+          style="width: 100px;margin-left: 5px;margin-right: 5px"
+      >
+        Add
+      </v-btn>
     </nav>
   </div>
 </template>
